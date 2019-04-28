4511046843c052347ced9b56af99be640277a886
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2019-04-28 17:32:36 +0200

    Add SFX + MainMenu script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("GameScenes");
+    }
+
+    public void OnClickStory()
+    {
+        SceneManager.LoadScene("StoryScene");
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
