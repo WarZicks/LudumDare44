8e084633f7a34394ad571579dbf751f6a733a87f
OwenMdvd <owen.mdvd@gmail.com>
2019-04-28 21:35:43 +0200

    ui

--- Assets/Scripts/TriggerEnd.cs
+++ Assets/Scripts/TriggerEnd.cs
@@ -7,13 +7,26 @@ public class TriggerEnd : MonoBehaviour
     [Tooltip("Le premier est le canavs actif")]
     public GameObject[] CanvasZone;
 
+    public GameObject[] Capacite;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             CanvasZone[0].SetActive(false);
-            CanvasZone[1].SetActive(true);
+
+            if (CanvasZone[1] != null)
+            {
+                CanvasZone[1].SetActive(true);
+            }
             LevelManager.instance.ChangeLevel(1);
+            if(Capacite !=null)
+            {
+                foreach (GameObject go in Capacite)
+                {
+                    go.SetActive(true);
+                }
+            }
         }
     }
 }
